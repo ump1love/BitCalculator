758fb5bb10bdbdbb1833d85caa6fbc195c54bec3
ump1love <steam.max22.d74.74@gmail.com>
2023-10-04 16:12:20 +0300

    BitCalculator

---
+++ BitCalculator/Calculator.cs
@@ -0,0 +1,66 @@
+﻿
+
+class Calculator
+{
+
+    public char UserSignChoice()
+    {
+        char choice;
+
+        Console.WriteLine("\nChoose sign: +, -, *, / :");
+        Console.Write("Choice: ");
+        choice = Console.ReadKey().KeyChar;
+
+        return choice;
+    }
+
+    public int UserCountingSystemChoice(char choiceSign)
+    {
+        int choice;
+
+        Console.WriteLine("\nChoose what counting system do you want to use: \n" +
+                          $"1 - Bit(X2 {choiceSign} X2) \n" +
+                          $"2 - Octal(X8 {choiceSign} X8) \n" +
+                          $"3 - Decimal(X10 {choiceSign} X10) \n" +
+                          $"4 - Hexadecimal (X16 {choiceSign} X16) \n" +
+                          "5 - Back");
+        Console.Write("Choice: ");
+        choice = int.Parse(Console.ReadLine());
+
+
+        return choice;
+    }
+
+    public void GenericCalculator(char choiceSign, int countingSystemBase)
+    {
+        Console.Write($"\nEnter first number (X{countingSystemBase}): ");
+        string input1 = Console.ReadLine();
+
+        Console.Write($"\nEnter second number (X{countingSystemBase}): ");
+        string input2 = Console.ReadLine();
+
+        try
+        {
+            int num1 = Convert.ToInt32(input1, countingSystemBase);
+            int num2 = Convert.ToInt32(input2, countingSystemBase);
+
+            int result = 0;
+
+            switch (choiceSign)
+            {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/':
+                    if (num2 != 0) { result = num1 / num2; }
+                    else { Console.WriteLine("Error: Division by zero"); return; }
+                    break;
+                default: Console.WriteLine("Invalid sign choice"); return;
+            }
+
+            Console.WriteLine($"{input1} {choiceSign} {input2} = {Convert.ToString(result, countingSystemBase)} (X{countingSystemBase})");
+        }
+        catch (FormatException){ Console.WriteLine("Invalid input format for the chosen counting system."); }
+    }
+
+}
---
+++ BitCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+class Program
+{
+
+    public static void Main()
+    {
+        Calculator calculator = new Calculator();
+        start:
+        char signChoice;
+        do
+        {
+            signChoice = calculator.UserSignChoice();
+
+        }while( signChoice != '-' && signChoice != '+' && signChoice != '/' && signChoice != '*');
+        int systemChoice = calculator.UserCountingSystemChoice(signChoice);
+
+        switch (systemChoice)
+        {
+            case 1: calculator.GenericCalculator(signChoice, 2); break; 
+            case 2: calculator.GenericCalculator(signChoice, 8); break; 
+            case 3: calculator.GenericCalculator(signChoice, 10); break;
+            case 4: calculator.GenericCalculator(signChoice, 16); break;
+            case 5: goto start;
+            default: Console.WriteLine("\nInvalid input"); break;
+        }
+    }
+
+}
